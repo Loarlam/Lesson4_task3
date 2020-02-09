b039fa31f29eb42e2735516425a8d871f33fd1f4
Loarlam <messiah02@mail.ru>
2020-02-09 12:29:57 +0300

    Add project files.

---
+++ Task3/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Player : IPlayable, IRecodable
+    {
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Record()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
